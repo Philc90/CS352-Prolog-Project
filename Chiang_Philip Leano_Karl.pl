01e9a6fb8809ca2188f68dee44aa4b873d3bf53b
Philip Chiang <plchiang@cpp.edu>
2017-05-26 09:35:35 -0700

    Modifications to grammar, also test file from the notes

--- Chiang_Philip Leano_Karl.pl
+++ Chiang_Philip Leano_Karl.pl
@@ -5,25 +5,63 @@ Statements:
 Questions: Interrogative(Determiner)
 Both: Determiner, Quantifier(Determiner), Verb, Noun/Pronoun, Preposition
 
+Q's:
+The color of the car is blue. =?= The color of a car is blue. =?= The color of this car is blue. =?= The color of my car is blue.
+How to link DCG with facts
+
 */
+
+%% execute(S, E) :- s(S, M), write("I know.").
+
+%%=============================================================================
+
+%% Fact is in the database.
+%% execute(S, E) :- S = [the, Prop, of, the, Obj, is, Prop_val], fact(Obj, Prop, Prop_val),
+%%    write("I know."), !.
+
+%% Conflicting fact is in the database.
+%% execute(S, E) :- S = [the, Prop, of, the, Obj, is, Prop_val], fact(Obj, Prop, Actual_prop_val),
+%%    format("That's not true. The ~w of the ~w is ~w.", [Prop, Obj, Actual_prop_val]), !.
+
+%% Object is in the database, but user wants to define new property & property value.
+%% execute(S, E) :- S = [the, Prop, of, the, Obj, is, Prop_val], fact(Obj, Existing_prop, _),
+%%    Existing_prop \= Prop, assert(fact(Obj, Prop, Prop_val)), write("OK."), !.
+
+%% Fact isn't in database
+%% execute(S, E) :- S = [the, Prop, of, the, Obj, is, Prop_val],
+%%    fact(Existing_obj, _, _), Existing_obj \= Obj,
+%%    assert(fact(Obj, Prop, Prop_val)), write("OK."), !.
+
+%% User entered something weird
+%% execute(_,_) :- write("I don't know."), !.
+
+%%=============================================================================
 
 s --> statement. %% | question.
 
-statement --> np, prep_p, vp.
+statement --> p(property), prep_p, vp.
+%% Thing is each type of phrase: object, property, or property_value
+p(Thing) --> det, n(Thing) | n(Thing).
 
-np --> det, n | n.
-
-vp --> v, np.
 %% Preposition phrase e.g. "of the car"
-prep_p --> prep, np.
+prep_p --> prep, p(object).
 
-%% Interrogative Determiner
-inter_det --> [what].
+vp --> v, p(property_value).
 
 det --> [the] | [a].
 
-n --> [color] | [car] | [blue].
+n(object) --> [car].
+
+n(property) --> [color].
+
+n(property_value) --> [blue].
 
 v --> [is].
 %% Preposition
 prep --> [of].
+
+%% Interrogative Determiner
+inter_det --> [what].
+
+%% fact(object, property, property_value)
+fact(car, color, blue).
